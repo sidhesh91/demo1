da5179c030e20224318b9800ad7fa17ffcbead89
sidhesh91 <sidhesh91@gmail.com>
2021-01-18 15:19:37 +0530

    Update first.bat

--- first.bat
+++ first.bat
@@ -1,1 +1,1 @@
-curl -u admin:Hello@1234 -i -X POST http://localhost:8082/artifactory/api/search/aql -H "Content-Type: text/plain" -d "items.find({ \"repo\": {\"$eq\" : \"example-repo-local\"}})"
+curl -u admin:Hello@1234 -i -X POST http://localhost:8082/artifactory/api/search/aql -H "Content-Type: text/plain" -d "items.find({\"type\":\"folder\",\"repo\":{\"$eq\":\"example-repo-local\"},\"path\":{\"$eq\":\"dir1\"}}).include(\"name\")" 
